61545e2e04e3ec0052c2c3f2afdf3ec327e1d061
shashank <shashank@leanmvp.co>
2024-12-26 04:42:56 +0530

    No changes detected; empty commit.

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:canvas_app/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark, // Ensure brightness matches
+        ),
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: const Home(),
+    );
+  }
+}
