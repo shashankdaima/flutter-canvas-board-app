9d13f15b0fa7206312c2f55514f6ceb320314e49
shashank <shashank@leanmvp.co>
2024-12-26 05:58:23 +0530

    Integrate EditModeProvider to display current edit mode in PageContent widget

--- lib/page_content.dart
+++ lib/page_content.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'edit_mode_provider.dart';
 
 class PageContent extends StatelessWidget {
   const PageContent({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final currentMode = Provider.of<EditModeProvider>(context).currentMode;
+    // print('Current Edit Mode: ${currentMode ?? 'None'}');
+
     return Container(
       decoration: BoxDecoration(
         color: Colors.white,
@@ -18,7 +23,7 @@ class PageContent extends StatelessWidget {
       ),
       child: Center(
         child: Text(
-          'Drawing Area',
+         currentMode?.toString().split('.').last ?? "None",
           // style: theme.textTheme.bodyLarge?.copyWith(
           //   color: Colors.black,
           // ),
