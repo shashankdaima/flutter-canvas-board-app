ce54bfae6c63f2c1bcb43b1e92854d375038900a
shashank <shashank@leanmvp.co>
2024-12-30 12:19:56 +0530

    erasor is working

--- lib/utils/erasor_collision_util_function.dart
+++ lib/utils/erasor_collision_util_function.dart
@@ -1,7 +1,9 @@
 import "package:flutter/material.dart";
 
 import "../models/drawing_elements/drawing_element.dart";
+import "../models/drawing_elements/image_element.dart";
 import "../models/drawing_elements/pencil_element.dart";
+import "../models/drawing_elements/text_element.dart";
 
 // Helper method to check if a point is near a path
 bool isPointNearPath(Offset point, Path path, double threshold) {
@@ -29,6 +31,28 @@ List<String> findElementsToErase(
     if (element is PencilElement) {
       if (isPointNearPath(point, element.path, eraserWidth / 2)) {
         elementsToErase.add(element.id);
+      }
+    } else if (element is ImageElement) {
+      if (element.bounds.contains(point)) {
+        elementsToErase.add(element.id);
+      }
+    } else if (element is TextElement) {
+      final textPainter = TextPainter(
+        text: TextSpan(text: element.content, style: TextStyle(fontSize: 16.0)),
+        textDirection: TextDirection.ltr,
+      )..layout();
+
+      final textBounds = element.bounds;
+      final textOffset = Offset(textBounds.left, textBounds.top);
+
+      for (double dx = 0; dx < textPainter.width; dx += 1.0) {
+        for (double dy = 0; dy < textPainter.height; dy += 1.0) {
+          final textPoint = textOffset + Offset(dx, dy);
+          if ((textPoint - point).distance < eraserWidth / 2) {
+            elementsToErase.add(element.id);
+            break;
+          }
+        }
       }
     }
   }
